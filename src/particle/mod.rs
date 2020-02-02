e62a5bd6b0d6ae71ccf30350af90b066b08924f2
tgblackburn <tomblackburn@gmail.com>
2020-02-02 14:53:53 +0100

    Stop checking for photon absorption after delay
    Most photon absorption events occur shortly after emission - to save runtime, stop checking for photons once a certain interval has elapsed.

--- src/particle/mod.rs
+++ src/particle/mod.rs
@@ -518,6 +518,7 @@ pub fn emit_radiation(e: &mut Population<Electron>, ph: &mut Population<Photon>,
 #[allow(unused)]
 pub fn absorb(e: &mut Population<Electron>, ph: &mut Population<Photon>, t: f64, dt: f64, xmin: f64, dx: f64, max_displacement: Option<f64>) {
     const PHOTON_E_ECRIT_CUTOFF: f64 = 1.0e-8;
+    const PHOTON_ESCAPE_TIME: f64 = 1.0e-15;
 
     if e.store.is_empty() || ph.store.is_empty() {
         return;
@@ -546,6 +547,10 @@ pub fn absorb(e: &mut Population<Electron>, ph: &mut Population<Photon>, t: f64,
 
                 for photon in chunk.iter_mut() {
                     if photon.chi() * ELECTRON_MASS_MEV / photon.energy() < PHOTON_E_ECRIT_CUTOFF {
+                        continue;
+                    }
+
+                    if t - photon.birth_time() > PHOTON_ESCAPE_TIME {
                         continue;
                     }
 
